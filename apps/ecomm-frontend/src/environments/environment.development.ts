e14ccdbd85995c4254beccd993ef999993452dca
acx394 <osamah.rafae@haw-hamburg.de>
2025-10-17 20:17:28 +0200

    add environments

---
+++ apps/ecomm-frontend/src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environmentDevelopment = {
+  kinde: {
+    clientId: '6321d32ae8fc4961bddbdf95ddf46102',
+    redirectUri: 'http://localhost:4200',
+    postLogoutRedirectUri: 'http://localhost:4200',
+    audience: 'http://localhost:8080/api',
+  },
+  apiUrl: 'http://localhost:8080/api',
+};
