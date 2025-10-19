691a22f511346d3a4d2ef2ca5a122b920f28a7c6
acx394 <osamah.rafae@haw-hamburg.de>
2025-10-19 13:03:47 +0200

    add navbar functions

--- apps/ecomm-frontend/src/app/layout/navbar/navbar.ts
+++ apps/ecomm-frontend/src/app/layout/navbar/navbar.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
-
+import {Oauth2Service} from "../../auth/oauth2-service";
+import {faUser, faUserCircle} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'ecomm-navbar',
   imports: [CommonModule, FaIconComponent],
@@ -9,4 +10,42 @@ import {FaIconComponent} from "@fortawesome/angular-fontawesome";
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  faUser = faUser;
+
+  oauth2Service = inject(Oauth2Service);
+
+
+  connectedUserQuery :any;
+  ngOnInit() {
+    this.connectedUserQuery = this.oauth2Service;
+  }
+
+
+  closeDropDownMenu() {
+  const bodyElement = document.activeElement  as HTMLBodyElement;
+  if (bodyElement) {
+    bodyElement.blur();
+  }
+  }
+
+  login(): void {
+    this.closeDropDownMenu();
+    this.oauth2Service.login();
+
+  }
+
+
+  logout(): void {
+    this.closeDropDownMenu();
+    this.oauth2Service.logout();
+  }
+
+  isConnected(): boolean {
+    return this.oauth2Service.connectedUserQuery?.data()?.email !== this.oauth2Service.noConnected &&
+    this.oauth2Service.connectedUserQuery?.status()==='success';
+  }
+
+
+  protected readonly faUserCircle = faUserCircle;
+}
